7dedc226d627272a0caead322907dd89f69fde4a
Jessica Santos <jm.jeska@gmail.com>
2022-06-06 21:16:58 -0300

    Adicionando rota para o componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'entrar', component: LoginComponent },
+  { path: 'cadastrar', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
