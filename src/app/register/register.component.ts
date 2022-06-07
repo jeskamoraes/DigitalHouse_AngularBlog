5bf2f8ba7af299a68e534a139e915dc8a0fcb514
Jessica Santos <jm.jeska@gmail.com>
2022-06-06 21:40:07 -0300

    Implementando métodos confirmarSenha(), tipoUser() e cadastrar()

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,10 @@
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  userLogin: UserLogin = new UserLogin
+  confirmarSenha:string
+  tipoUsuario:string 
+ 
+  
+  constructor(
+    private authService:AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  confirmSenha(event:any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event:any) {
+    this.tipoUser = event.target.value
+  }
+
+  cadastrar(
+  ) {
+    this.user.tipo = this.tipoUsuario // Criar atributo tipo nas models e colocar no backend
+
+    if(this.user.senha != this.confirmarSenha)
+    {
+      alert('As senhas não conferem')
+    }
+    else{
+      this.authService.cadastrar(this.user).subscribe((resp: User)=> {
+        this.user = resp
+        this.router.navigate(['/entrar']) // Rota da próxima tela que deve ser aberta após o cadastro
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
   }
 
 }
