76d0b3bca9ebf0319471d84ea5b3693fe3aba1d1
Jessica Santos <jm.jeska@gmail.com>
2022-06-06 21:38:10 -0300

    Implementando método entrar()

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,7 @@
+import { environment } from './../../environments/environment.prod';
+import { Router } from '@angular/router';
+import { AuthService } from './../service/auth.service';
+import { UserLogin } from './../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe((resp:UserLogin) => {
+      this.userLogin = resp
+      
+      // Variáveis globais
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+
+      // Apenas para visualizar as infos
+      console.log(environment.token)
+      console.log(environment.nome)
+      console.log(environment.foto)
+      console.log(environment.id)
+
+      this.router.navigate(['/home'])
+    }, erro => {
+      if(erro.status == 500) {
+        alert('Usuário e senha incorretos')
+      }
+      
+    }
+    )
   }
 
 }
