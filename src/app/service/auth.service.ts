24344f89172dad85aba87afc159c4c8a3ce061fb
Jessica Santos <jm.jeska@gmail.com>
2022-06-08 20:10:01 -0300

    Implementando método para verificar se o usuário está logado

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable'; // Conferir se é esse import
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -17,7 +18,17 @@ export class AuthService {
 		return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
 
+  // Método para salavar o cadastro do usuário
   cadastrar(user:User):Observable<User> {
 		return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', User)
 	}
+
+  // Método responsável por verificar se o usuário está logado
+  logado() {
+    let ok: boolean = false
+    if(environment.token != '') {
+      ok = true;
+    }
+    return ok
+  }
 }
