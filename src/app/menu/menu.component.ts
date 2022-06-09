82f4639a67daeb3b16300338803facf1d5688d5b
Jessica Santos <jm.jeska@gmail.com>
2022-06-08 21:17:45 -0300

    Implementando método sair e adicionando as variáveis de ambiente

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  // No menu do professor aparecem o nome e foto do user logado
+  // Informações serão carregadas quando o usuário logar
+  nome = environment.nome
+  foto = environment.foto
+
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  sair() {
+    // Redirecionando para a página de login
+    this.router.navigate(['/entrar']) 
+
+    // Zerando todos os atributos do usuário que deslogou
+    environment.token = ''
+    environment.nome = ''
+    environment.foto = ''
+    environment.id = 0
+
+  }
 }
